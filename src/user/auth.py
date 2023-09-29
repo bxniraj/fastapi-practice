57ec8ba8372278421c1bad2f45951026a7451e53
Niraj Patel <bxniraj@gmail.com>
2023-09-29 10:27:24 +0530

    FINAL COMMIT

--- src/user/auth.py
+++ src/user/auth.py
@@ -1,13 +1,23 @@
 from sqlalchemy.orm import Session
 from src.user import models , schemas
 from fastapi import  HTTPException , Depends, Header
-from database import get_db , SECRET_KEY , ALGORITHM , PASSWORD_REGEX , EMAIL_REGEX
+from database import get_db
 from passlib.context import CryptContext 
 from datetime import timedelta , datetime
 import re , jwt
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+refresh_token_expire_minutes = 60 * 24 * 7
+
+SECRET_KEY = "cda6ea7fd9a2da89721288a94d1d0b8af57a5cd29df2c8e852e42f4a5a3c30fd"
+
+ALGORITHM = "HS256"
+
+EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+
+PASSWORD_REGEX = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
 
 # Login
 def login(data: schemas.User, db: Session = Depends(get_db)):
@@ -28,8 +38,10 @@ def login(data: schemas.User, db: Session = Depends(get_db)):
 
         access_token = create_token(data={"sub": user.email})
 
+        refresh_token = create_refresh_token(data={"sub": user.email})
+
         return {'access_token' : access_token,
-                # 'refresh_token' : refresh_token,
+                'refresh_token' : refresh_token,
                 'token_type': 'Bearer' }
     else:
         raise HTTPException(status_code=401, detail="Incorrect email or password")
@@ -38,10 +50,19 @@ def login(data: schemas.User, db: Session = Depends(get_db)):
 # Function to generate JWT token
 def create_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=10)
+    expire = datetime.utcnow() + timedelta(minutes=1)
     to_encode.update({"exp": expire})
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return token
+
+# Refresh Token
+def create_refresh_token(data:dict):
+    to_encode = data.copy()
+    expire = timedelta(minutes=refresh_token_expire_minutes) + datetime.utcnow()
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
 
 # Decode Tokens
 def decode_token(authorization: str = Header(), db: Session = Depends(get_db)):
@@ -66,3 +87,27 @@ def decode_token(authorization: str = Header(), db: Session = Depends(get_db)):
     
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
+def refresh_access_token(refresh_token: str = Header(), db: Session = Depends(get_db)):
+        
+    try:
+        # Verify the refresh token
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        email = payload.get("sub")
+
+        # Assuming you have a function to fetch the user by email from the database
+        user = db.query(models.User).filter(models.User.email == email).first()
+
+        if user:
+            # Generate a new access token
+            access_token = create_token(data={"sub": user.email})
+            return {"access_token": access_token}
+        else:
+            raise HTTPException(status_code=401, detail="User not found")
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token has expired")
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
